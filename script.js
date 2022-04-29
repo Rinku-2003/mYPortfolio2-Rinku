3c313d3938e19edf332505877a5fc23fe9e787d0
Rinku-2003 <101516110+Rinku-2003@users.noreply.github.com>
2022-04-29 20:54:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student","Developer", "Engineer", "Designer", ],
+        strings: ["Student","Developer", "Designer", ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
